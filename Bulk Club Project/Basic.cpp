0f4493d9f228993b0262b1f4cbcbb09141bff90b
scubastew <scubastew@users.noreply.github.com>
2014-04-27 16:50:40 -0700

    Update Basic.cpp

--- Bulk Club Project/Basic.cpp
+++ Bulk Club Project/Basic.cpp
@@ -1,7 +1,7 @@
 /*************************************************************************
  * AUTHOR		: Andrea Velazquez & Justin Stewart
- * STUDENT ID	: 994759
- * FINAL PROJECT: Super Warehouse Store
+ * STUDENT ID		: 994759
+ * FINAL PROJECT	: Super Warehouse Store
  * CLASS 		: CS1C
  * SECTION		: TTH: 8:30a - 9:50a
  * DUE DATE		: May 13th, 2014
