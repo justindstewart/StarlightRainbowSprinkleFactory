3bb542fb4164b87dcc3379299113db6725849186
andorito <andreacrochets@gmail.com>
2014-05-12 14:39:36 -0700

    Create SortVectorByItemName.cpp

---
+++ Bulk Club Project/SortVectorByItemName.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+ * AUTHOR		: Andrea Velazquez & Justin Stewart
+ * STUDENT ID	: 838042 & 994759
+ * FINAL PROJECT: Super Warehouse Store
+ * CLASS 		: CS1C
+ * SECTION		: TTH: 8:30a - 9:50a
+ * DUE DATE		: May 13th, 2014
+ ************************************************************************/
+
+#include "header.h"
+
+History temp;
+
+void SortVectorByItemName(vector <History> &histVector)
+{
+	unsigned vectorSize = histVector.size();
+
+	for(unsigned iterator = 1; iterator< vectorSize; iterator++)
+	{
+		for (unsigned index = 0; index < (vectorSize - iterator); index++)
+		{
+			if(histVector[index].getName() > histVector[index+1].getName())
+			{
+				temp = histVector[index];
+				histVector[index]= histVector[index+1];
+				histVector[index+1] = temp;
+			}
+		}
+	}
+}
